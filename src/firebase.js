da9c280d1035a67312d1f3621dbda8be6051b559
Munthasir-10 <munthasir1010@gmail.com>
2023-11-06 19:52:52 +0530

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxCioMDX8L-xXlQXIOxRRE0FplzNw7ERI",
+  authDomain: "hoo-bank-d1297.firebaseapp.com",
+  projectId: "hoo-bank-d1297",
+  storageBucket: "hoo-bank-d1297.appspot.com",
+  messagingSenderId: "977857447712",
+  appId: "1:977857447712:web:a5104f1d99b14f459f7945"
+};
+
+const app = initializeApp(firebaseConfig);
